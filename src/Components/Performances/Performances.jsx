039869bee47989839745b1bcf637e2cecfb06bb8
Kuper Bank <KuperBank@Tommi-Roses-15-MacBook-Pro-2.local>
2024-11-13 15:18:22 -0800

    added performanceData.js and added it to performance.jsx

--- src/Components/Performances/Performances.jsx
+++ src/Components/Performances/Performances.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
 import './Performances.css'
+import performances from '../../assets/performancesData.js'
 
 const Performances = () => {
   return (
-    <div>Performances</div>
+    <div id='performances' className='performances'>
+      <div className="title-box">
+        <h1>My Performances</h1>
+        
+      </div>
+      <div className="performances-container">
+        {performances.map((performance,index)=>{
+          return <div key={index} className='performances-format'>
+             <h3>{performance.date}</h3>
+             <h2>{performance.event}</h2>
+             <p>{performance.location}</p>
+              <p>{performance.details}</p>
+          </div>
+        })}
+      </div>
+    </div>
   )
 }
 
---
+++ src/assets/performancesData.js
@@ -0,0 +1,46 @@
+const performances = [
+  {
+    date: "June 5",
+    event: "Recital at MonteCedro",
+    location: "Altadena, CA",
+    details: "With Milos Veljkovic, piano (Music by Schumann, Beethoven, Faure, Bruch, and Dvorak)"
+  },
+  {
+    date: "June 9-11",
+    event: "Mainly Mozart Festival",
+    location: "San Diego, CA",
+    details: ""
+  },
+  {
+    date: "June 14-26",
+    event: "Sunflower Festival",
+    location: "Topeka, KS",
+    details: ""
+  },
+  {
+    date: "July 6",
+    event: "Concert in Amsterdam",
+    location: "Amsterdam",
+    details: "Schubert C Major Quintet"
+  },
+  {
+    date: "July 12-17",
+    event: "Buzzards Bay Festival",
+    location: "Marion, MA",
+    details: ""
+  },
+  {
+    date: "July 18-30",
+    event: "Idyllwild Summer Program ChamberFest",
+    location: "Idyllwild, CA",
+    details: "Pacific Trio in residence"
+  },
+  {
+    date: "October 15",
+    event: "Santa Monica Symphony",
+    location: "Santa Monica, CA",
+    details: "Brahms Double Concerto with Andrew Sords"
+  }
+];
+
+export default performances;
